75ff2e2b9115eb04656ae0fca4dfde1cac6a8f20
Siddharth <114420294+Sidtheogdeveloper@users.noreply.github.com>
2025-07-19 12:04:37 +0530

    Fix adjacent JSX elements error in UserPortal

--- src/components/UserPortal.tsx
+++ src/components/UserPortal.tsx
@@ -275,6 +275,9 @@ const UserPortal: React.FC<UserPortalProps> = ({ onNavigate }) => {
   }
 
   return (
+    <>
+    <>
+    <>
     <div className="min-h-screen bg-gray-50 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
@@ -500,6 +503,9 @@ const UserPortal: React.FC<UserPortalProps> = ({ onNavigate }) => {
         </div>
       </div>
     </div>
+    </>
+    </>
+    </>
   );
 };
 
